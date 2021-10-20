130a7a2b42fc1bf4dc4583f93701d6c1d536d7fa
klapmax <klapan1602@gmail.com>
2021-10-20 21:56:54 +0300

    corrected

--- lab2/task2.4.py
+++ lab2/task2.4.py
@@ -1,4 +1,5 @@
 class BINARYTREE:
+    # initialization of tree, required checkings
     def __init__(self, code, price):
         self.left = None
         self.right = None
@@ -9,32 +10,32 @@ class BINARYTREE:
         if price < 0 or code < 0 or not code or not price:
             raise ValueError("incorrect value")
 
-
-    def put_data(self, code, price):
+    # movement on our tree
+    def get_data(self, code, price):
         if self.code:
             if code == self.code:
-                raise ValueError("incorrect date")
+                raise ValueError("incorrect data")
             if code < self.code:
                 if self.left is None:
                     self.left = BINARYTREE(code, price)
                 else:
-                    self.left.put_data(code, price)
+                    self.left.get_data(code, price)
             elif code > self.code:
                 if self.right is None:
                     self.right = BINARYTREE(code, price)
                 else:
-                    self.right.put_data(code, price)
+                    self.right.get_data(code, price)
         else:
             self.code = code
-
+    
     def get_price(self, code):
         if code < self.code:
             if self.left is None:
-                raise ValueError("not found")
+                raise ValueError("Error")
             return self.left.get_price(code)
         elif code > self.code:
             if self.right is None:
-                raise ValueError("not found")
+                raise ValueError("Error")
             return self.right.get_price(code)
         else:
             return self.price
@@ -46,13 +47,13 @@ class BINARYTREE:
 
 
 object = BINARYTREE(1, 46)
-object.put_data(2, 54)
-object.put_data(3, 46)
-object.put_data(4, 300)
-object.put_data(5, 16)
-object.put_data(6, 91)
-object.put_data(7, 812)
-object.put_data(8, 16)
-object.put_data(9, 32)
-object.put_data(10, 254)
+object.get_data(2, 54)
+object.get_data(3, 46)
+object.get_data(4, 300)
+object.get_data(5, 16)
+object.get_data(6, 91)
+object.get_data(7, 812)
+object.get_data(8, 16)
+object.get_data(9, 32)
+object.get_data(10, 254)
 print('Total value: ' + str(object.cost(7, 44)))
