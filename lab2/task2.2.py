a87d2ea44421824d9eb95f3cbd15afd08ce70643
klapmax <klapan1602@gmail.com>
2021-10-21 11:41:22 +0300

    something is reworked

--- lab2/task2.2.py
+++ lab2/task2.2.py
@@ -1,15 +1,21 @@
 import re
+import os.path
 class Process:
     def __init__(self, name_of_file):
-        file = open(name_of_file, "r")
-        self.text = file.read()
-        file.close()
+        if not isinstance(name_of_file, str):
+            raise TypeError("incorrect type")
+        elif not os.path.exists(name_of_file):
+            raise FileNotFoundError("file not found")
+        self.name_of_file = name_of_file
 
     def function(self):
-        signs = len(self.text) 
-        words = len(re.findall(r'[a-zA-Z-\']+', self.text))
-        sentences = len(re.split(r'[.!?]+', self.text)) - 1
-        return f'Signs: {signs} \nWords: {words} \nSentences: {sentences}' 
+        file = open(self.name_of_file, "r")
+        self.text = file.read()
+        __signs = len(self.text) 
+        __words = len(re.findall(r'[a-zA-Z-\']+', self.text))
+        __sentences = len(re.split(r'[.!?]+', self.text)) - 1
+        file.close()
+        return f'Signs: {__signs}\nWords: {__words}\nSentences: {__sentences}' 
     
 object = Process("text.txt")
 try:
