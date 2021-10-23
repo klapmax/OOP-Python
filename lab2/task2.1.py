d7201b7573f0f2d3a8309cd2a6f0ace8f41eeeb2
klapmax <klapan1602@gmail.com>
2021-10-23 16:41:15 +0300

    reworked and modifyed

--- lab2/task2.1.py
+++ lab2/task2.1.py
@@ -1,16 +1,23 @@
+from posixpath import isabs
+
+
 class Product:
     def __init__(self, name_of_product, price, dimension, description):
-        self.__name = name_of_product
-        self.price = price
-        self.__dimension = dimension
-        self.__description = description
+        if isinstance(name_of_product, str) and isinstance(price, (float, int)) and isinstance(dimension, str) and isinstance(description, str):
+            if name_of_product.isalpha() and isinstance(price, (float, int)):
+                self.__name = name_of_product
+                self.price = price
+                self.__dimension = dimension
+                self.__description = description
 
 class Customer:
     def __init__(self, surname, name, patronymic, mobile_phone):
-        self.__surname = surname
-        self.__name = name
-        self.__patronymic = patronymic
-        self.__mobile_phone = mobile_phone
+        if isinstance(name, str) and isinstance(surname, str) and isinstance(patronymic, str) and isinstance(mobile_phone, str):
+            if name.isalpha() and surname.isalpha() and patronymic.isalpha():
+                self.__surname = surname
+                self.__name = name
+                self.__patronymic = patronymic
+                self.__mobile_phone = mobile_phone
 
 class Order():
     def __init__(self, customer, *products):
@@ -24,8 +31,9 @@ class Order():
         return cost
 
 Maks = Customer('Klapatiuk', 'Maksym', 'Volodymyrovych', '+380687046036')
-Xiaomi = Product('Cellphone', 15000, 'L = 16, W = 8', 'black')
-Sennheiser = Product('Headphones', 8000, 'Weight = 100 g, Height = 3', 'black-blue')
-order = Order(Maks, Xiaomi, Sennheiser)
+Xiaomi = Product('Cellphone',15000, 'L = 12, W = 8', 'black')
+Sennheiser = Product('Headphones',8000, 'Weight = 100 g, Height = 3', 'black-blue')
+Nokia = Product('Phone',3700, 'Weight = 115 g, Height = 9', 'red')
+shop_order = Order(Maks, Xiaomi, Sennheiser, Nokia)
 
-print(order.method())
+print(shop_order.method())
--- lab2/task2.2.py
+++ lab2/task2.2.py
@@ -8,17 +8,31 @@ class Process:
             raise FileNotFoundError("file not found")
         self.name_of_file = name_of_file
 
-    def function(self):
+    def for_signs(self):
         file = open(self.name_of_file, "r")
         self.text = file.read()
-        __signs = len(self.text) 
+        __signs = len(self.text)
+        file.close()
+        return f'Signs: {__signs}'
+
+    def for_words(self):
+        file = open(self.name_of_file, "r")
+        self.text = file.read()
         __words = len(re.findall(r'[a-zA-Z-\']+', self.text))
+        file.close()
+        return f'Words: {__words}'
+
+    def for_sentences(self):
+        file = open(self.name_of_file, "r")
+        self.text = file.read()
         __sentences = len(re.split(r'[.!?]+', self.text)) - 1
         file.close()
-        return f'Signs: {__signs}\nWords: {__words}\nSentences: {__sentences}' 
-    
-object = Process("text.txt")
+        return f'Sentences: {__sentences}'
+
+object1 = Process("text.txt")
 try:
-    print(object.function())
+    print(object1.for_words())
+    print(object1.for_signs())
+    print(object1.for_sentences())
 except:
-    print("Incorrect data!")
+    TypeError("Incorrect data!")
